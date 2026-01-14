aa95a0e3c7d8378704ce655a7c0e87be69380a57
rotpfau-prog <rotpfau@gmail.com>
2026-01-14 12:51:14 +0300

    Update authorization URL to use HTTPS

--- src/components/OmadaPortal.tsx
+++ src/components/OmadaPortal.tsx
@@ -23,7 +23,7 @@ export default function OmadaPortal() {
     // If Omada parameters exist, authorize
     if (clientMac && site) {
       // Construct authorization URL
-      const authUrl = `http://192.168.50.1:8080/portal/auth`;
+      const authUrl = `https://aps1-omada-essential-controller.tplinkcloud.com`;
       
       // Create authorization form data
       const formData = new URLSearchParams({
